24c6d04eddc57c25888d01a0c2ec1440717f7dde
pablo.pentreath <pablo.pentreath@bue.edu.ar>
2021-05-14 07:28:27 -0300

    first commit

---
+++ src/Controller/API/Products.php
@@ -0,0 +1,15 @@
+<?php
+
+
+namespace App\Controller\API;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class Products extends AbstractController
+{
+
+    public function sendProducts(){
+
+    }
+}
---
+++ src/Service/API/Porducts/Validator/ProductsValidator.php
@@ -0,0 +1,12 @@
+<?php
+
+
+namespace App\Service\API\Porducts\Validator;
+
+
+class ProductsValidator
+{
+    public static function validate(){
+
+    }
+}
